b0da867024aa8024062d6be931cf4b65738fe08c
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-01-21 22:33:49 +0300

    First commit. Project created and first look on browser successful.

---
+++ src/test/java/com/springinaction/TacoCloudApplication/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springinaction.TacoCloudApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
