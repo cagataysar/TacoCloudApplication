9e9a878dd1c5ad4c3e82d4157419b20df1b08d31
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2023-01-29 11:52:38 +0300

    spring boot validation added to models and controllers

--- src/main/java/com/springinaction/TacoCloudApplication/controller/DesignTacoController.java
+++ src/main/java/com/springinaction/TacoCloudApplication/controller/DesignTacoController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +40,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+        if ( errors.hasErrors() ) {
+            return "design";
+        }
+
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
 
